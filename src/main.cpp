2f395a43f06944314e47163943ca3840557b8445
Todd Rylaarsdam <todd@toddr.org>
2023-05-03 17:43:54 -0500

    compiling

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
